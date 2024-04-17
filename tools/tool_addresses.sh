410d330781bc3a12ae0f33a0ed8d0103e78fa49a
Filip Filmar <fmil@google.com>
2024-04-16 20:11:22 -0700

    feat: rearrange

--- tools/tool_addresses.sh
+++
@@ -1,7 +0,0 @@
-# THIS IS A GENERATED FILE.
-# You can edit, but you can also revert to the original version by running:
-#    bazel run @bazel_local_nix//:install
-
-NIX_PORTABLE_BINARY="external/nix_portable/file/downloaded"
-BAZEL_WRAPPER="bazel-out/host/bin/tools/bazel_wrapper"
-
